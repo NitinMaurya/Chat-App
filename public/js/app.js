c8771bb18e5e0728e55d9b6f1d030f08471782dd
Nitin Maurya <nitin.maurya1997@gmail.com>
2017-01-22 20:25:01 +0530

    Added sending live data bvack and forth

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,10 @@
 var socket = io();
+
 socket.on('connect',function (){
     console.log('User is Connected!');
+});
+
+socket.on('message',function (message){
+    console.log('New Message : ');
+    console.log(message);
 });
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 /**
  * Created by nitin on 21/1/17.
  */
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
-io.on('connection',function (){
+io.on('connection',function (socket){
     console.log('User Connected via Socket.io!');
+    socket.emit('message',{
+        text : 'Welcome to the Chat application!!'
+    });
+    socket.on('message',function (message){
+        console.log('Message Received : ' +  message.text);
+        socket.broadcast.emit('message',message);
+    });
 });
 http.listen(PORT,function (){
     console.log('Server is running at port ' + PORT + '....' );
