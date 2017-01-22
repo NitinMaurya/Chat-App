b2d32237caaf08152c5af13d1a70c4f0a7108454
Nitin Maurya <nitin.maurya1997@gmail.com>
2017-01-22 13:12:18 +0530

    Added client socket.io

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 /**
  * Created by nitin on 21/1/17.
  */
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
-app.listen(PORT,function (){
+io.on('connection',function (){
+    console.log('User Connected via Socket.io!');
+});
+http.listen(PORT,function (){
     console.log('Server is running at port ' + PORT + '....' );
 });
